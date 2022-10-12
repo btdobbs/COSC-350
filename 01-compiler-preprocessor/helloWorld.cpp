6d69dc540c87424a2061f710ada1eaf29d103b7c
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-10-12 09:28:09 -0400

    Update helloWorld.cpp

--- 01-compiler-preprocessor/helloWorld.cpp
+++ 01-compiler-preprocessor/helloWorld.cpp
@@ -1,1 +1,11 @@
+//# used for preprocessor statement or things that are done to a file before it's sent to the c++ compiler
+//link in the system library iostream 
+#include <iostream>
 
+int main () {
+  //link in the user file includeme.cpp
+  #include "includeme.cpp"
+  //including iostream allows the use of std::cout
+  std::cout << "Hello World!\n";
+  return 0;
+}
