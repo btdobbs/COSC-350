9de24b2d33ae937c1125e5bd04cc19335e15ccbe
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-10-12 20:21:26 -0400

    Update main.cpp

--- 08-enumerations/main.cpp
+++ 08-enumerations/main.cpp
@@ -1,1 +1,41 @@
+#include <iostream>
 
+using namespace std;
+
+int main ()
+{
+  //enum associates a user friendly value that can be used in code
+  //first value defaults to 0 and subsequent values are incremented by 1
+  enum weekend {saturday, sunday};
+  //you can force the first value to be a specific number and subsequent values are incremented by 1
+  //Note, numbers can be negative but they can not collide.  Forexample, setting A to 9 would generate a compile error
+  enum TestType {A=3,B,C,D=10,E,F};
+  for(int i = 0; i<15; i++)
+  {
+    switch ((TestType)i)
+    {
+      case A:
+        cout << "A=i=" << i << "\n";
+        break;
+      case B:
+        cout << "B=i=" << i << "\n";
+        break;
+      case C:
+        cout << "C=i=" << i << "\n";
+        break;
+      case D:
+        cout << "D=i=" << i << "\n";
+        break;
+      case E:
+        cout << "E=i=" << i << "\n";
+        break;
+      case F:
+        cout << "F=i=" << i << "\n";
+        break;
+      default:
+        cout << "default=i=" << i << "\n";
+        break;
+    }
+  }
+  return 0;
+}
