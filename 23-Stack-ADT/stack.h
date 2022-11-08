627894ef74dc1f9e1d5e41369aa121ac7c175c78
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-08 18:41:29 -0500

    Update stack.h

--- 23-Stack-ADT/stack.h
+++ 23-Stack-ADT/stack.h
@@ -1,1 +1,12 @@
-
+#include "Element.h"
+//LIFO (last in first out)
+//cafeteria plates, pez, web, undo
+//use virtual keyword to behave more like an interface
+class Stack{
+public:
+    void push(Element e);  // add element to top of stack
+    void pop(); // remove element from top of stack (error if empty)
+    Element top(); // reference element on top of stack (error if empty)
+    int size(); // size of stack
+    bool empty(); //true if stack is empty
+};
