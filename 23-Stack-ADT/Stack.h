1d488572efd8a51c876c289169cd7ffe32ccc242
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-09 08:21:21 -0500

    Update Stack.h

--- 23-Stack-ADT/Stack.h
+++ 23-Stack-ADT/Stack.h
@@ -1,12 +1,13 @@
 #include "Element.h"
 //LIFO (last in first out)
-//cafeteria plates, pez, pringles, web, undo
-//use virtual keyword to behave more like an interface
+//cafeteria plates, pez, web, undo
+//use virtual keyword in C++ to behave more like a c#/java interface
+//python - just class, javascript - prototype (nothing to enforce in these two)
 class Stack{
 public:
-    void push(Element e);  // add element to top of stack
-    void pop(); // remove element from top of stack (error if empty)
-    Element top(); // reference element on top of stack (error if empty)
-    int size(); // size of stack
-    bool empty(); //true if stack is empty
+    virtual void push(Element e) = 0;  // add element to top of stack
+    virtual void pop() = 0;            // remove element from top of stack (error if empty)
+    virtual Element top() = 0;         // reference element on top of stack (error if empty)
+    virtual int size() = 0;            // size of stack
+    virtual bool empty() = 0;          //true if stack is empty
 };
