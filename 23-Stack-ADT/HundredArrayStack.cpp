ca33c8f89118b37ece4962ae77ae1cfc778d9e52
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-10 09:55:56 -0500

    Update HundredArrayStack.cpp

--- 23-Stack-ADT/HundredArrayStack.cpp
+++ 23-Stack-ADT/HundredArrayStack.cpp
@@ -1,1 +1,22 @@
+#include "HundredArrayStack.h"
 
+void HundredArrayStack::push(Element e) {
+a[_size] = e;
+_size++;
+}
+
+void HundredArrayStack::pop() {
+_size--;
+}
+
+Element HundredArrayStack::top() {
+return a[_size-1];
+}
+
+int HundredArrayStack::size() {
+return _size;
+}
+
+bool HundredArrayStack::empty() {
+return _size == 0;
+}
