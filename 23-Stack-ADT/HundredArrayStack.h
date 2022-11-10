e2937c23a4b307b4e155ace3d8b84fbbe9f2bed4
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-10 09:56:31 -0500

    Update HundredArrayStack.h

--- 23-Stack-ADT/HundredArrayStack.h
+++ 23-Stack-ADT/HundredArrayStack.h
@@ -5,25 +5,9 @@ private:
     int _size = 0;
     Element a[100]={};
 public:
-    void push(Element e) override {
-        a[_size] = e;
-        _size++;
-    }
-
-    void pop() override {
-        _size--;
-    }
-
-    Element top() override {
-        return a[_size-1];
-    }
-
-    int size() override {
-        return _size;
-    }
-
-    bool empty() override {
-        return _size == 0;
-    }
-
+    void push(Element e) override;
+    void pop() override;
+    Element top() override;
+    int size() override;
+    bool empty() override;
 };
