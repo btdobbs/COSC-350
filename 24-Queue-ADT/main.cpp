348c1893beea53eef4b11dcec8f1034b521fe006
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-10 19:36:35 -0500

    Update main.cpp

--- 24-Queue-ADT/main.cpp
+++ 24-Queue-ADT/main.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include "ArrayQueue.h"
+//#include "ListQueue.h"
 
+using namespace std;
+
+int main(){
+    ArrayQueue queue;
+    Element e;
+    char option;
+    do{
+        cout << queue.to_string() << endl;
+        cout << "Size = " << queue.size() << endl;
+        cout << "Empty = " << queue.empty() << endl;
+        cout << "Enter option: (d)equeue, (e)nqueue, e(x)it" << endl;
+        cin >> option;
+        switch (option) {
+            case 'd':
+                queue.dequeue();
+                break;
+            case 'e':
+                queue.enqueue(e);
+                break;
+            case 'x':
+                break;
+            default:
+                cout << option << " is not a valid option." << endl;
+                break;
+        }
+    }while (option != 'x');
+}
