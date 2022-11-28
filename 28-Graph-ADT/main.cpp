3ea752d559f1aeb5bcfbfc344fcd5b265f121443
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-11-27 19:49:18 -0500

    Update main.cpp

--- 28-Graph-ADT/main.cpp
+++ 28-Graph-ADT/main.cpp
@@ -1,1 +1,16 @@
+#include "Graph.h"
+#include <iostream>
+#include <random>
 
+using namespace std;
+
+int main() {
+    Graph *graph;
+    Element e1;
+    Vertex *v1 = graph->insert_vertex(e1);
+    Element e2;
+    Vertex *v2 = graph->insert_vertex(e2);
+    Element e3;
+    Edge *e = graph->insert_edge(v1, v2, e3);
+    return 0;
+}
