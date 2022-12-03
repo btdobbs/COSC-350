60b104525da37132369faf419bfd47604e354f23
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-12-03 10:28:25 -0500

    Update main.cpp

--- 29-Map-ADT/main.cpp
+++ 29-Map-ADT/main.cpp
@@ -1,1 +1,14 @@
+#include "Map.h"
+#include <iostream>
+#include <random>
 
+using namespace std;
+
+int main() {
+    Map *map;
+    Element key;
+    Element value;
+    Iterator position;
+    position = map->put(key, value);
+    position = map->find(key);
+}
