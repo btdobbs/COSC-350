15f8b6d02aa55de1bebf9f0214baf3831c88c719
Todd Dobbs <5673867+btdobbs@users.noreply.github.com>
2022-12-13 16:08:51 -0500

    Update main.cpp

--- 23-Stack-ADT/main.cpp
+++ 23-Stack-ADT/main.cpp
@@ -7,8 +7,7 @@ using namespace std;
 void reverse(Vector v){
     HundredArrayStack stack;
     for (int i = 0; i < 10; i++) {
-        Element e;
-        stack.push(e);
+        stack.push(v.at(i));
     }
     for (int i = 0; i < 10; i++) {
         v.insert(i,stack.top());
